a0b020c7c0be3923a1befa0fab22a56143f9d165
fargelus <ddraudred@gmail.com>
2018-09-18 16:17:45 +0300

    Finished

--- lesson-2/NemykinDmitriy/homework.ts
+++ lesson-2/NemykinDmitriy/homework.ts
@@ -21,3 +21,33 @@ function summator(...operands: Operand[]): Operand {
   return operands.reduce((accumulator: Operand, current: Operand) => plus(accumulator, current));
 }
 
+
+// 3
+function getUnique(...array: any[]): any[] {
+  const anySet: Set<any> = new Set(array);
+  const uniqueElementsArray: any[] = [];
+
+  anySet.forEach((value: any) => uniqueElementsArray.push(value));
+  return uniqueElementsArray;
+}
+
+
+// 4
+function toMatrix(data: any[], rowSize: number): any[] {
+  if (rowSize <= 0) {
+    throw 'argument rowSize must be positive';
+  }
+
+  let it: number = 0;
+  const matrixSize: number = Math.ceil(data.length / rowSize);
+  const matrix: any[] = new Array(matrixSize).fill(undefined);
+
+  return matrix.map(() => {
+    const row: any[] = new Array(rowSize);
+
+    const slicedData: any[] = data.slice(it, it += rowSize);
+    slicedData.forEach((value: any, index) => row[index] = value);
+
+    return row;
+  });
+}
